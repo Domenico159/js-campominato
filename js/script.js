40dc21a3fc565fce85ceda9bfc40b3064038451d
Domenico159 <domenico.zummo@gmail.com>
2021-04-06 15:50:56 +0200

    add base file

---
+++ js/script.js
@@ -0,0 +1,72 @@
+
+
+var numeriTot = 100;
+
+var bombeTot = 16;
+
+var possibilita = numeriTot - bombe;
+
+var campo = document.getElementById('campo');
+
+var bombe = [];
+
+
+
+    
+
+
+  while(bombe.length < bombeTot){
+        
+    var randomNumber = numberRandom(1 , 100);
+
+       if(! bombe.includes(randomNumber)){
+          bombe.push(randomNumber);
+
+         }
+  }
+
+
+for(var i = 1 ; i <= numeriTot ; i++){
+
+    if(! bombe.includes(i)){
+        campo.innerHTML += '<li id ="ciao">' + i + '</li>' ;
+    }
+    else{
+        campo.innerHTML += '<li class="red">' + '</li>' ; 
+    }
+
+
+    var elemento = document.getElementById('ciao');
+
+elemento.addEventListener('click', 
+
+            function(){
+                
+                var thisElement = this;
+
+
+            })
+}
+
+
+/******************************************************
+ *  Functions
+ ******************************************************/ 
+
+function numberRandom(min,max){
+
+    return Math.floor(Math.random() * (max - min) +1) + min;
+
+
+}
+
+
+
+
+
+
+        
+
+
+
+
